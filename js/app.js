4cd9a64025a111501840d58d37660bc8b0f9ed75
Paulo Pereira <paulovvp@outlook.com>
2023-12-20 18:19:38 -0300

    correção

--- js/app.js
+++ js/app.js
@@ -3,47 +3,43 @@ function comprar(){
 
     //recuperar os tipos de ingresso e quantidade
     let ingresso = document.getElementById('tipo-ingresso').value;
-    let quantidade = document.getElementById('qtd').value;
+    let quantidade = parseInt(document.getElementById('qtd').value);
     //subtrair da quantidade restante e mostrar mensagem caso a quantidade desejada seja maior que os ingressos desejados
-    let lista = document.querySelector('.lista');
-    let qtdPista = document.getElementById('qtd-pista').textContent;
-    let qtdSuperior = document.getElementById('qtd-superior').textContent;
-    let qtdInferior = document.getElementById('qtd-inferior').textContent;
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
     
     switch (ingresso){
         case 'inferior':
-            if (qtdInferior - quantidade >= 0){
+            if (quantidade > qtdInferior){
                 qtdInferior = qtdInferior - quantidade;
-                lista.innerHTML = `<li>Pista<span id="qtd-pista">${qtdPista}</span></li>
-                                   <li>Cadeira superior<span id="qtd-superior">${qtdSuperior}</span></li>
-                                   <li>Cadeira inferior<span id="qtd-inferior">${qtdInferior}</span></li>`;
+                qtdInferior.document.getElementById('qtd-inferior').textContent = qtdInferior;
             }else{
-                alert(`quantidade indisponível para Cadeira Inferior!`); 
+                  alert(`quantidade indisponível para Cadeira Inferior!`); 
             }  
+                        
         break;
-        
+       
         case 'superior':
-            if (qtdSuperior - quantidade >= 0){
+            if (quantidade > qtdSuperior){
                 qtdSuperior = qtdSuperior - quantidade;
-                lista.innerHTML = `<li>Pista<span id="qtd-pista">${qtdPista}</span></li>
-                                   <li>Cadeira superior<span id="qtd-superior">${qtdSuperior}</span></li>
-                                   <li>Cadeira inferior<span id="qtd-inferior">${qtdInferior}</span></li>`;
+                qtdSuperior.document.getElementById('qtd-superior').textContent = qtdSuperior;
             }else{
-                alert(`quantidade indisponível para Cadeira Superior!`); 
-            }  
+                  alert(`quantidade indisponível para Cadeira Superior!`); 
+            }       
         break;   
+               
 
         case 'pista':
-            if (qtdPista - quantidade >= 0){
+            if (quantidade > qtdPista){
                 qtdPista = qtdPista - quantidade;
-                lista.innerHTML = `<li>Pista<span id="qtd-pista">${qtdPista}</span></li>
-                                   <li>Cadeira superior<span id="qtd-superior">${qtdSuperior}</span></li>
-                                   <li>Cadeira inferior<span id="qtd-inferior">${qtdInferior}</span></li>`;
+                qtdPista.document.getElementById('qtd-pista').textContent = qtdPista;
             }else{
                 alert(`quantidade indisponível para Pista!`); 
             }  
         break;                      
             
-    }  
+    
 
+    }  
 }
