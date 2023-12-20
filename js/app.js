3b342ef01159c5c394a30d6d707743a680263364
Paulo Pereira <paulovvp@outlook.com>
2023-12-20 18:32:13 -0300

    projeto concluido

--- js/app.js
+++ js/app.js
@@ -8,11 +8,8 @@ function comprar(){
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
     let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
     let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
-
-    console.log(quantidade, qtdPista)  
     
     switch (ingresso){
-
         case 'inferior':
             if (quantidade > qtdInferior){
                 alert(`quantidade indisponível para Cadeira Inferior!`); 
@@ -20,10 +17,9 @@ function comprar(){
                   qtdInferior = qtdInferior - quantidade;
                   document.getElementById('qtd-inferior').textContent = qtdInferior;
                   alert(`Compra realziada com sucesso!`);
-                }  
-                        
+                }             
         break;
-       
+
         case 'superior':
             if (quantidade > qtdSuperior){
                 alert(`quantidade indisponível para Cadeira Superior!`); 
@@ -32,11 +28,9 @@ function comprar(){
                 document.getElementById('qtd-superior').textContent = qtdSuperior;
                 alert(`Compra realziada com sucesso!`);
             }       
-
         break;   
                
         case 'pista':
-
             if (quantidade > qtdPista){
                 alert(`quantidade indisponível para Cadeira Superior!`); 
             }else{
@@ -44,7 +38,6 @@ function comprar(){
                 document.getElementById('qtd-pista').textContent = qtdPista;
                 alert(`Compra realziada com sucesso!`);
             } 
-
         break;                      
     }  
 }
